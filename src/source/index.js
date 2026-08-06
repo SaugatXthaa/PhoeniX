6272931305fbc7078790ea23cdc28c4cb9b3c64d
PhoeniX <phoenix@local>
2026-08-06 19:59:11 +0000

    Revert "Add AnimeKai source (animekai.uno/.cl) — sub + dub anime streams"
    This reverts commit 8b40f244bcf94fbfc74683ff0a0822df258ba381.

--- src/source/index.js
+++ src/source/index.js
@@ -4,7 +4,6 @@
 import { CineHDPlus } from './CineHDPlus.js';
 import { CineWave } from './CineWave.js';
 import { AnimeFlix } from './AnimeFlix.js';
-import { AnimeKai } from './AnimeKai.js';
 import { Cuevana } from './Cuevana.js';
 import { Einschalten } from './Einschalten.js';
 import { Eurostreaming } from './Eurostreaming.js';
@@ -56,7 +55,6 @@ export const createSources = (fetcher) => {
     new NineAnime(fetcher),
     new HiAnime(fetcher),
     new AnimeFlix(fetcher),
-    new AnimeKai(fetcher),
     // AL
     new Kokoshka(fetcher),
     // ES / MX
